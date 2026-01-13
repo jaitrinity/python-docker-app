481ab2dae80bfbc812522a3254881764aea7d4c6
Jai Trinity <jai.prakash@trinityapplab.co.in>
2026-01-13 13:02:38 +0530

    Final fix test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python Docker App on AWS EC2 , CI/CD 1234 🚀"
+    return "Hello from Python Docker App on AWS EC2 , CI/CD 5678 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
